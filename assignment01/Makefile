5cdd9f1d8f32005b8a54267915c0675c7aa09e98
Anirudh Lath <anirudhlath@gmail.com>
2022-01-17 16:56:19 -0700

    Restructing root

---
+++ assignment01/Makefile
@@ -0,0 +1,5 @@
+SRCS = main.cpp cmdline.cpp
+INCS = cmdline.h
+
+msdscript: $(SRCS) $(INCS)
+	$(CXX) --std=c++14 -O2 -o msdscript $(SRCS)
---
+++ assignment01/cmdline.h
@@ -0,0 +1,10 @@
+//
+// Created by aniru on 1/11/2022.
+//
+
+#ifndef MSDSCRIPT_CMDLINE_H
+#define MSDSCRIPT_CMDLINE_H
+
+int use_arguments(int argc, char **argv);
+
+#endif //MSDSCRIPT_CMDLINE_H
